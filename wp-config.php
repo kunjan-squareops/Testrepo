2520296a179ae6444857dfe3ed93518d95cb1c1d
kunj134 <114722786+kunj134@users.noreply.github.com>
2022-10-10 17:01:15 +0530

    Updated wp-config.php file
    Updated Wordpress config php file with default db configuration

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpressdb' );
+define( 'DB_NAME', 'database_name_here' );
 
 /** Database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', 'username_here' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'kunjanisadmin' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** Database hostname */
-define( 'DB_HOST', 'kunjan-rds-mysql1.cnbwakbdzeez.us-east-2.rds.amazonaws.com' );
+define( 'DB_HOST', 'localhost' );
 
 define('FS_METHOD', 'default');
 
